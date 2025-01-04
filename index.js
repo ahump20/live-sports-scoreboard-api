1ddbf80bad8291fd1f441c6e12ddf823b68ecd2e
Nishant Sethunath <snishk@gmail.com>
2025-01-03 16:44:01 -0800

    added and tested endpoints for MLB scores

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
 
-const redis_conn = new Redis({ host: REDIS_SERVER_IP, port: REDIS_SERVER_PORT });
-redis_conn.ping().then((res) => {
-    console.log('Redis connection successful:', res);
+const nfl_redis_conn = new Redis({ host: REDIS_SERVER_IP, port: REDIS_SERVER_PORT, db:0 });
+nfl_redis_conn.ping().then((res) => {
+    console.log('Redis connection to NFL DB is successful:', res);
+});
+
+const mlb_redis_conn = new Redis({ host: REDIS_SERVER_IP, port: REDIS_SERVER_PORT, db:1 });
+mlb_redis_conn.ping().then((res) => {
+    console.log('Redis connection to MLB DB is successful:', res);
 });
 
 app.get('/', async (req, res) => {
@@ -27,7 +32,7 @@ app.get('/debug', async (req, res) => {
 app.get('/test-redis-conn', async (req, res) => {
     const test_game_key = "test_game:001";
     try {
-        const test_game = await redis_conn.hgetall(test_game_key);
+        const test_game = await nfl_redis_conn.hgetall(test_game_key);
         console.log(test_game);
         res.status(200).send(test_game);
     } catch (error) {
@@ -38,7 +43,7 @@ app.get('/test-redis-conn', async (req, res) => {
 
 app.get('/nfl/get-game-count', async (req, res) => {
     try {
-        const nfl_game_count = await redis_conn.hgetall('nfl_game_count');
+        const nfl_game_count = await nfl_redis_conn.hgetall('nfl_game_count');
         console.log(nfl_game_count);
         res.status(200).json({game_count: nfl_game_count["game_count"]});
     } catch (error) {
@@ -50,7 +55,7 @@ app.get('/nfl/get-game-count', async (req, res) => {
 app.get('/nfl/get-live-score/:id', async (req, res) => {
     const id = req.params.id;
     try {
-        const game = await redis_conn.hgetall(`nfl_game:${id}`);
+        const game = await nfl_redis_conn.hgetall(`nfl_game:${id}`);
         const response_json = {
             away_team: game["away_team"],
             away_team_id: game["away_team_id"],
@@ -69,4 +74,41 @@ app.get('/nfl/get-live-score/:id', async (req, res) => {
     } catch (error) {
         res.status(500).send("Internal Server Error");
     }
-})
+});
+
+app.get('/mlb/get-game-count', async (req, res) => {
+    try {
+        const mlb_game_count = await mlb_redis_conn.hgetall('mlb_game_count');
+        console.log(mlb_game_count);
+        res.status(200).json({game_count: mlb_game_count["game_count"]});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Encountered an error while trying to get game count from DB: " + error);
+    }
+});
+
+const convertStringToBoolean = (value) => {
+    return (value.toLowerCase() === 'true');
+}
+
+app.get('/mlb/get-live-score/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const game = await mlb_redis_conn.hgetall(`mlb_game:${id}`);
+        const response_json = {
+            away_team: game["away_team"],
+            home_team: game["home_team"],
+            away_score: game["away_score"],
+            home_score: game["home_score"],
+            inning: game["inning"],
+            current_state: game["current_state"],
+            first_base: convertStringToBoolean(game["first_base"]),
+            second_base: convertStringToBoolean(game["second_base"]),
+            third_base: convertStringToBoolean(game["third_base"])
+        };
+        console.log(response_json);
+        res.status(200).json(response_json);
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
